a88fc7b63d4c74ddcd01c24b5b52403c096c42f2
KoksyFox <kobivval2001@gmail.com>
2022-05-04 12:59:47 +0300

    added dsp labs 3-5
    added dsp labs 3-5, delete some part of second lab

--- dsp/lab_02/lab_02.m
+++ dsp/lab_02/lab_02.m
@@ -29,36 +29,4 @@ zg2 = fftshift(zx2);
 
 M = 0:length(t)-1;
 
-figure (1);
-plot(M,abs(yx1)/length(M),'r',M,abs(yg1)/length(M),'black');
-title('FFT: амплитудный спектр прямоугольного импульса');
-legend('С эффектом близнецов','Без эффекта близнецов');
 
-figure(2);
-plot(M,abs(yx2)/length(M),'r',M,abs(yg2)/length(M),'black');
-title('FFT: амплитудный спектр Гауссова импульса');
-legend('С эффектом близнецов','Без эффекта близнецов');
-
-figure (3);
-plot(M,abs(zx1)/length(M),'r',M,abs(zg1)/length(M),'black');
-title('DFT: амплитудный спектр прямоугольного импульса');
-legend('С эффектом близнецов','Без эффекта близнецов');
-
-figure (4);
-plot(M,abs(zx2)/length(M),'r',M,abs(zg2)/length(M),'black');
-title('DFT: амплитудный спектр Гауссова импульса');
-legend('С эффектом близнецов','Без эффекта близнецов');
-end
-
-% Дискретное преобразование Фурье
-function y = dft(x)
-a = 0:length(x)-1;
-b = -2 * pi * sqrt(-1) * a / length(x);
-for i = 1:length(a)
-    a(i) = 0;
-    for j = 1:length(x)
-        a(i) = a(i) + x(j) * exp(b(i) * j);
-    end
-end
-y = a;
-end
